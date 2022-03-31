33a2f88afb55e70996e6a7eccf2396f11db74f2a
Eason <easonkongys@gmail.com>
2022-03-31 12:22:12 +0800

    d

--- backend/control.js
+++ backend/control.js
@@ -1,3 +1,5 @@
+var Loader;
+
 function snackbar(message) {
     var x = document.getElementById("snackbar");
     if (message === null || message === ""){
@@ -7,3 +9,24 @@ function snackbar(message) {
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3200);
   }
+
+function firstLoad() {
+
+    if (typeof(Storage) !== "undefined") {
+        fload = localStorage.getItem("firstLoad")
+        if (fload === "true") {
+            showPage();
+        } else {
+            localStorage.setItem("firstLoad", "true");
+            Loader = setTimeout(showPage, 3000);
+        }
+    } else {
+        window.location.replace("/error?error=unsupported_storage");
+    }
+    
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("loader-bg").style.display = "none";
+}
