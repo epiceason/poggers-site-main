d50eabb2cc9f3db63404a135223a2f25ed7c0a99
Eason <easonkongys@gmail.com>
2022-04-30 14:56:40 +0800

    t

--- read/watch.js
+++ read/watch.js
@@ -1,11 +1,23 @@
 async function main() {
+
+    // Settings ////////////////////////////////////////////////////////////////////
+
+    var enabled = false;
+
+    ////////////////////////////////////////////////////////////////////////////////
+
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const id = urlParams.get('v')
 
+    if (enabled !== true) {
+        window.location.replace("./");
+    }
+
     if (id === null || id === "") {
-        document.getElementById("title_display").innerText = "View Unavailable";
-        document.getElementById("information_display_bar").innerText = "More than enough views | More than enough favourites | 1-1-1970";
-        document.getElementById("information_description").innerText = "It seems like this viewing thing is unavailable. Please try again later or return to the main menu.";
+        window.location.replace("/invalid");
+        // document.getElementById("title_display").innerText = "View Unavailable";
+        // document.getElementById("information_display_bar").innerText = "More than enough views | More than enough favourites | 1-1-1970";
+        // document.getElementById("information_description").innerText = "It seems like this viewing thing is unavailable. Please try again later or return to the main menu.";
     }
 }
